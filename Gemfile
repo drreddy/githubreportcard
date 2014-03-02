02a74c324887e08c2cb78e063e6e57e449155163
Rajeev Reddy <rajeev.reddy.d@gmail.com>
2014-03-02 18:12:28 +0530

    github report card app added

--- Gemfile
+++ Gemfile
@@ -1,12 +1,21 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-ruby '1.9.3'
+gem 'rails', '3.2.16'
 
-gem 'rails'
-
-gem 'thin'
-gem 'faye'
+group :development, :test do
+	gem 'sqlite3'
+end
 
 group :production do
-  gem 'pg'
+	gem 'pg'
 end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubreportcard::Application.config.secret_token = '255e56cd79ededcbf2a1fc21660767f3d4c59ab4ff8a34b0a0da3c973157efaf221896e3a637ead2636b50ce8acd41338bebd61906bfe4c84d70ac706e6e6b58'
